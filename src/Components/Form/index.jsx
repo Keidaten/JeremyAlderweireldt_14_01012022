5a08dd07ae988153e90dd5dcf855fbe18e410506
Jey <jeremyalder@hotmail.fr>
2022-01-26 15:50:33 +0100

    added localstorage when form submit

--- src/Components/Form/index.jsx
+++ src/Components/Form/index.jsx
@@ -3,10 +3,33 @@ import DropDown from '../Dropdown';
 import DatePickerComponent from '../DatePickerComponent';
 import TextInput from '../TextInput';
 
+//redux
+import { useSelector } from 'react-redux';
+
 function Form() {
+	const store = useSelector((state) => state);
+
+	console.log(store);
+
+	//local storage
+	const updateEmployees = () => {
+		//get localStorage and parse it
+		let storage = JSON.parse(localStorage.getItem('Employee'));
+		console.log(storage);
+		//ig storage null, initialize store as a table, if not : push storage to store
+		storage === null ? (storage = [store]) : storage.push(store);
+		//set localStorage with jsoned storage
+		localStorage.setItem('Employee', JSON.stringify(storage));
+	};
+
+	const handleSumbit = (e) => {
+		e.preventDefault();
+		updateEmployees();
+	};
+
 	return (
 		<>
-			<form action="#" id="create-employee">
+			<form action="#" id="create-employee" onSubmit={(e) => handleSumbit(e)}>
 				<TextInput id="firstName" label="First Name" />
 				<TextInput id="lastName" label="Last Name" />
 				<DatePickerComponent id="dateOfBirth" />
